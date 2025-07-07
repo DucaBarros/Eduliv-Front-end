527ce096b72d91ff6317257b4278615932204c14
Eduardo Barros <eduardocarneiro83@gmail.com>
2025-07-07 00:25:37 -0300

    Curso Front-end Java Aula 12 Javascript - concluída

---
+++ Front-end/Aula 12 - Javascript/script.js
@@ -0,0 +1,42 @@
+// My scripts
+
+// console.log
+// console.log('Hello World!')
+// console.error('Error 404')
+// console.warn('Everything is alright!')
+
+// typeof
+// let num1 = "2"
+// console.log(typeof num1)
+
+//String() + typeof()
+// let numString = 2
+// let string2 = String(numString)
+// console.log(typeof string2)
+
+//typeof
+// let num2 = 2
+// console.log(typeof num2)
+
+//typeof
+// let num3 = 0.2
+// console.log(typeof num3)
+
+// prompt & three ways of declaring variables
+// prompt always get strings
+// const number1 = parseFloat(prompt('Digite o primeiro número')) // parseFloat() convert to float
+// var number2 = parseFloat(prompt('Digite o segundo número'))
+// let number3 = parseFloat(prompt('Digite o terceiro número'))
+// let sumResult = number1 + number2 + number3
+
+// const number1 = Number(prompt('Digite o primeiro número')) // Number() convert to float
+// var number2 = Number(prompt('Digite o segundo número'))
+// let number3 = Number(prompt('Digite o terceiro número'))
+// let sumResult2 = number1 + number2 + number3
+// console.log(sumResult2)
+
+//Using alert
+// alert(sumResult)
+
+
+
